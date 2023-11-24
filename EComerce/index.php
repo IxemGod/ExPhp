abbf4357d4cf1d5c7d468efa4d61fb7ce416b3e1
IxemGod <68782318+IxemGod@users.noreply.github.com>
2023-11-24 12:10:30 +0100

    Ajout des trucs pour que Alan soit content

--- EComerce/index.php
+++ EComerce/index.php
@@ -1,89 +1,34 @@
-<?php include "header.php"?>
-
-<link type="text/css" rel="stylesheet" href="css/style.css">
-<script src="https://kit.fontawesome.com/fcb8c34dd8.js" crossorigin="anonymous"></script>
-<body>
-   <section>
-    <h2>BOUTIQUE Coutellerie en Ligne</h2>
+<?php include "header.php" ?>
+<section class="produits">
+    <h1>BOUTIQUE Coutellerie en Ligne</h1>
 
     <div class="marque">
         <p>Couteaux CATERPILLAR</p>
     </div>
 
     <div class="list-item">
-    <?php
-    // ID, Nom Ref, description, prix, img
-    foreach ($_SESSION['article'] as $data) {?>
-        <div class="produit">
-            <span><?=$data["ref"]?> - <?=$data["name"]?> <i class="fa-regular fa-heart"></i></span>
-            <img src="<?=$data["img"]?>">
-            <p class="prix"><?=$data["prix"]?>€</p>
-            <div class="DP">
-                <a class="detail" href="article.php?id=<?=$data["id"]?>">Détails</a>
-                <a class="panier" href="panierAdd.php?id=<?=$data['id']?>&q=1">Ajouter au Panier</a>
-            </div>
+        <?php
 
-            <p class="stock"><i class="fa-solid fa-check" style="color: #11ff00;"></i> EN STOCK</p>
-        </div>
+        // Permet de désactiver les notif et les warning de php
+        ini_set('display_errors', 0);
+
+        // ID, Nom Ref, description, prix, img
+        // Ici on parcour la liste des article contenu dans $_SESSION['article'] et on les affiches de
+        // de manière stucturé
+        foreach ($_SESSION['article'] as $data) { ?>
+            <div class="produit">
+                <span><?= $data["ref"] ?> - <?= $data["name"] ?></span>
+                <img src="<?= $data["img"] ?>">
+                <p class="prix"><?= $data["prix"] ?>€</p>
+                <div class="DP">
+                    <a class="detail" href="article.php?id=<?= $data["id"] ?>">Détails</a>
+                    <a class="panier" href="panierAdd.php?id=<?= $data['id'] ?>&quantity=1">Ajouter au Panier</a>
+                </div>
+
+                <p class="stock"><i class="fa-solid fa-check" style="color: #11ff00;"></i> EN STOCK</p>
+            </div>
         <?php
         }
         ?>
-
-
-
-        
-<!--
-        <div class="produit">
-            <span>CAT91C80CP - Couteau CATERPILLAR <i class="fa-regular fa-heart"></i></span>
-            <img src="https://www.couteaux-center.com/Files/132354/Img/08/cat80cp-z.jpg">
-            <p class="prix">18.20€</p>
-            <div class="DP">
-                <button class="detail">Détails</button>
-                <button class="panier">Ajouter au Panier</button>
-            </div>
-
-            <p class="stock"><i class="fa-solid fa-check" style="color: #11ff00;"></i> EN STOCK</p>
-        </div>
-
-        <div class="produit">
-            <span>CAT91CAG46CP - Couteau CATERPILLAR
-                <i class="fa-regular fa-heart"></i></span>
-            <img src="https://www.couteaux-center.com/Files/132354/Img/18/cat46-z.jpg">
-            <p class="prix">18.95€</p>
-            <div class="DP">
-                <button class="detail">Détails</button>
-                <button class="panier">Ajouter au Panier</button>
-            </div>
-
-            <p class="stock"><i class="fa-solid fa-check" style="color: #11ff00;"></i> EN STOCK</p>
-        </div>
-
-        <div class="produit">
-            <span>CAT91C176 - Couteau CATERPILLAR Air Frame <i class="fa-regular fa-heart"></i></span>
-            <img src="https://www.couteaux-center.com/Files/132354/Img/19/cat176-z.jpg">
-            <p class="prix">48.90€</p>
-            <div class="DP">
-                <button class="detail">Détails</button>
-                <button class="panier">Ajouter au Panier</button>
-            </div>
-
-            <p class="stock"><i class="fa-solid fa-check" style="color: #11ff00;"></i> EN STOCK</p>
-        </div>
-
-        <div class="produit">
-            <span>CAT91C176 - Couteau CATERPILLAR Air Frame <i class="fa-regular fa-heart"></i></span>
-            <img src="https://www.couteaux-center.com/Files/132354/Img/19/cat176-z.jpg">
-            <p class="prix">48.90€</p>
-            <div class="DP">
-                <button class="detail">Détails</button>
-                <button class="panier">Ajouter au Panier</button>
-            </div>
-
-            <p class="stock"><i class="fa-solid fa-check" style="color: #11ff00;"></i> EN STOCK</p>
-        </div>
-    </div> -->
-    </section>
-</body>
-
-<?php include("footer.html");?>
-</html>
+</section>
+<?php include("footer.html"); ?>
--- EComerce/panier.php
+++ EComerce/panier.php
@@ -1,35 +1,79 @@
-<?php include("header.php");?>
-<link type="text/css" rel="stylesheet" href="css/panier.css">
-	<script src="https://kit.fontawesome.com/fcb8c34dd8.js" crossorigin="anonymous"></script>
-<body>
+<?php include("header.php"); ?>
+<section class="TablePanier">
 
-<section>
-    <h1> Votre Panier :</h1>
+<?php 
+    // $NbrCommand sert à compter le nombre d'article contenu dans le panier
+    $NbrCommand = 0;
+    foreach ($_SESSION['panier'] as $idCommand => $ArticleFromPan) {
+        $NbrCommand++;
+    }
 
-    <div class="articles">
-        <table>
-            <tr class="head"><th></th><th>Article</th><th>Quantité</th><th>Prix</th><th></th></tr>
+    if($NbrCommand > 0){
+        ?>
+        <h1> Votre Panier :</h1>
+
+        <div class="articles">
+            <table>
+                <tr class="head">
+                    <th></th>
+                    <th>Article</th>
+                    <th>Quantité</th>
+                    <th>Prix</th>
+                    <th></th>
+                </tr>
             <?php
+        }
+        else{
+            ?>
+            <h1>Le panier est vide tah ton avenir</h1>
+            <?php
+           }
+
+            // Fonction qui gère la modification de quantité depuis le panier
+            for ($i = 0; $i <= 6; $i++) {
+                if (isset($_POST[$i])) {
                     foreach ($_SESSION['panier'] as $idCommand => $ArticleFromPan) {
-                        // Article From pan contient : id, quantity
-                        // IdCommand contient l'id de la commande
-         
-                        // echo $ArticleFromPan;
-                        foreach ($_SESSION['article'] as $data) {
-                            if($data["id"] == $ArticleFromPan["id"])
-                            {
-                                echo "<tr><th> <img src='".$data["img"]."'></th>";
-                                echo "<th> ".$data["ref"]." - ".$data["name"]."</th>";
-                                echo "<th>".$ArticleFromPan["quantity"]."</th>";
-                                echo "<th>".$ArticleFromPan["quantity"]*$data['prix']."€</th>";
-                                echo "<th><a href='panierSupr.php?supr=".$idCommand."'><i class='fa-solid fa-square-xmark' style='color: #ff0000; padding:20px;'></i></a></th></tr>";
+                        if ($ArticleFromPan['id'] == $i) {
+                            $NewQuanti = htmlspecialchars($_POST[$i]);
+                            if ($NewQuanti <= 0) {
+                                $NewQuanti = 1;
                             }
+                            $_SESSION['panier'][$idCommand] = array("id" => $i, "quantity" => $NewQuanti);
+                        }
                     }
                 }
+            }
+
+            
+            
+            // Fonction qui affiche le panier complet sous forme de <table> HTML
+            foreach ($_SESSION['panier'] as $idCommand => $ArticleFromPan) {
+                // Article From pan contient : id, quantity
+                // IdCommand contient l'id de la commande
+
+                foreach ($_SESSION['article'] as $data) {
+                    if ($data["id"] == $ArticleFromPan["id"]) {
+                    ?>
+                        <tr>
+                            <th> <img src='<?= $data["img"] ?>'></th>
+                            <th><?= $data["ref"] ?> - <?= $data["name"] ?></th>
+                            <th>
+                                <form method='POST' action="panier.php"><input name='<?= $data['id'] ?>' type='number' value='<?= $ArticleFromPan["quantity"] ?>'> <button type='submit' name='submit'>Modifier</button></form>
+                            </th>
+                            <th><?=$ArticleFromPan["quantity"] * $data['prix']?>€</th>
+                            <th><a href='panierSupr.php?supr=<?= $idCommand ?>'><i class='fa-solid fa-square-xmark' style='color: #ff0000; padding:20px;'></i></a></th>
+                        </tr>
+                    <?php
+                    }
+                   
+                }
+                
+            }
+
             ?>
 
         </table>
     </div>
 
 </section>
-<?php include("footer.html");?>
+<?php include("footer.html"); ?>
